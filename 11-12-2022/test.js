1fb19d8f7e75f4ee97ff1bfa1ddb2b33dfde45c4
ShadaabIqbal <iqbalshadaab@gmail.com>
2022-12-11 17:40:47 +0530

    OK

---
+++ 11-12-2022/test.js
@@ -0,0 +1,43 @@
+//Qno1:
+// let N1 = 10
+// let N2 = 20
+
+// function sum(a,b) {
+//     let sum = a+b
+//     let R = sum
+//     return R
+// }
+
+// console.log(sum(N1, N2))
+
+//Qno2:
+// let a = 3
+// let b = 4
+
+// function sum(a,b) {
+//     let obj1 = {
+//         a: a
+//     }
+//     let obj1Dup = obj1.a
+
+//     let obj2 = {
+//         b: b
+//     }
+//     let obj2Dup = obj2.b
+//     let result = obj1Dup+obj2Dup
+//     return result
+// }
+
+// console.log(sum(a,b));
+
+//Qno3:
+// let rad = 100
+
+// function area(a) {
+// let pi = 3.14
+// let num = parseFloat(a).toFixed(2)
+// let area = pi*num*num
+// return parseFloat(area).toFixed(2)  
+// }
+
+// console.log(area(rad));
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Code-Zinger
+LIST OF QUESTIONS: 
+
+Qno1: Add 2 numbers.
+Given two integers N1 and N2, find their sum and store in R.
+The code to read N1 and N2 as well as to print R has already been provided.
+
+Qno2: Add two numbers using call by reference
+Given two numbers as input, add the two numbers using call by reference.
+
+Qno3: Given an integer radius R, find the area of a circle.
+Area of circle = pi * radius * radius.
+Where, pi is mathematical constant whose value is 3.14
+
