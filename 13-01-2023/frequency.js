c945c1d078263752d2b2b2fdbd6a1334231b45e1
ShadaabIqbal <iqbalshadaab@gmail.com>
2023-01-17 04:06:54 +0530

    DONE

--- 13-01-2023/frequency.js
+++ 13-01-2023/frequency.js
@@ -6,27 +6,27 @@
 // let arr1 = [2, 3, 4, 5, 6];
 // let arr2 = [4, 9, 16, 25, 36];
 // function isPresent(a, b) {
-//     let obj1 = {}
-//     let obj2 = {}
-//     if (a.length != b.length) { return false };
-//     for (let i in a) {
-//         if (!obj1[a[i]]) {
-//             obj1[a[i]] = 1;
-//         } else {
-//             obj1[a[i]]++;
-//         }
-//     }
-//     for (let i in b) {
-//         if (!obj2[b[i]]) {
-//             obj2[b[i]] = 1;
-//         } else {
-//             obj2[b[i]]++;
-//         }
-//     }
-//     for (let i in obj1) {
-//         if (obj2[i * i] != obj1[i]) return false;
-//     }
-//     return true;
+    // let obj1 = {}
+    // let obj2 = {}
+    // if (a.length != b.length) { return false };
+    // for (let i in a) {
+    //     if (!obj1[a[i]]) {
+    //         obj1[a[i]] = 1;
+    //     } else {
+    //         obj1[a[i]]++;
+    //     }
+    // }
+    // for (let i in b) {
+    //     if (!obj2[b[i]]) {
+    //         obj2[b[i]] = 1;
+    //     } else {
+    //         obj2[b[i]]++;
+    //     }
+    // }
+    // for (let i in obj1) {
+    //     if (obj2[i * i] != obj1[i]) return false;
+    // }
+    // return true;
 // }
 
 // console.log(isPresent(arr1, arr2));
@@ -37,27 +37,27 @@
 // Space complexity: O(1)
 
 // function isAnagram(a, b) {
-//     let count1 = new Array(26);
-//     let count2 = new Array(26);
+    // let count1 = new Array(26);
+    // let count2 = new Array(26);
 
-//     if (a.length != b.length) { return false }
+    // if (a.length != b.length) { return false }
 
-//     for (let i = 0; i < count1.length; i++) {
-//         count1[i] = 0;
-//         count2[i] = 0;
-//     }
+    // for (let i = 0; i < count1.length; i++) {
+    //     count1[i] = 0;
+    //     count2[i] = 0;
+    // }
 
-//     for (let i = 0; i < a.length && b.length; i++) {
-//         count1[a[i].toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0)]++
-//         count2[b[i].toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0)]++
-//     }
+    // for (let i = 0; i < a.length && b.length; i++) {
+    //     count1[a[i].toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0)]++
+    //     count2[b[i].toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0)]++
+    // }
 
-//     for (let i = 0; i < 26; i++) {
-//         if (count1[i] != count2[i]) {
-//             return false;
-//         }
-//     }
-//     return true;
+    // for (let i = 0; i < 26; i++) {
+    //     if (count1[i] != count2[i]) {
+    //         return false;
+    //     }
+    // }
+    // return true;
 // }
 
 // console.log(isAnagram('azz', 'azz'));
@@ -111,4 +111,6 @@
 // }
 
 // console.log(freq(b));
+
+
 
